5c92d5bdfad03a24087adc8516d419425c25dd11
4ndroid3 <andijaimo@gmail.com>
2021-04-18 23:16:27 -0300

    se agregan pruebas e info de middlewares

---
+++ posts/middleware.py
@@ -0,0 +1,16 @@
+from ipware import get_client_ip
+from django.http import HttpResponse
+
+BLACK_LIST = []
+
+class IPIsValid():
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip, is_routable = get_client_ip(request)
+
+        if ip in BLACK_LIST:
+            return HttpResponse('Bad request', status=404)
+        else:
+            return self.get_response(request)
--- pruebas_middleware/settings.py
+++ pruebas_middleware/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'posts.middleware.IPIsValid',
 ]
 
 ROOT_URLCONF = 'pruebas_middleware.urls'
